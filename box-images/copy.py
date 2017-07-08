35bcbc0007ad7f1b078d0e34dad53372659ed95c
abhishek <abhishek.gujrathi@totelec.in>
2017-07-08 19:56:23 +0530

    Images corrected

--- box-images/copy.py
+++ box-images/copy.py
@@ -1,12 +1,12 @@
 import os
 
-a=249
+a=1
 
-while a<297:
+while a<300:
 	print "copying"
 	print a
 
-	command = "cp box.jpg "+"box-"+str(a)+".jpg"
+	command = "cp box-correct.jpg "+"box-"+str(a)+".jpg"
 	print command
 	os.system(command)
 	a=a+1
